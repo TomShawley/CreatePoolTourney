495056041dcc26d73e41789e18c8cce167cdd1fc
TomShawley <thomas.shawley1@gmail.com>
2019-02-15 15:02:01 +0000

    Beans added to application class

--- src/main/java/com/qa/Pool/PoolApplication.java
+++ src/main/java/com/qa/Pool/PoolApplication.java
@@ -23,6 +23,26 @@ public class PoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PoolApplication.class, args);
 	}
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+	
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 	
 	
 
